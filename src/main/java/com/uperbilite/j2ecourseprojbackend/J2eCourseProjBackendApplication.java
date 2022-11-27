2f9dbf85007832525e70c422a9477e806c8432c8
uperbilite <uperbilite@gmail.com>
2022-11-28 01:31:06 +0800

    init proj

---
+++ src/main/java/com/uperbilite/j2ecourseprojbackend/J2eCourseProjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uperbilite.j2ecourseprojbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2eCourseProjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2eCourseProjBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uperbilite/j2ecourseprojbackend/J2eCourseProjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uperbilite.j2ecourseprojbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2eCourseProjBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
